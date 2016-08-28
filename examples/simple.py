eb0870eaa83a43b56a9697f5f01f789959d1b005
Åke Forslund <ake.forslund@gmail.com>
2016-08-28 11:22:15 +0200

    Give speech time to finish before terminating
    Delay added after speech is written to output device

--- examples/simple.py
+++ examples/simple.py
@@ -5,6 +5,7 @@ from pyaudio import PyAudio
 from pymimic import *
 
 from os.path import realpath, dirname
+from time import sleep
 
 voice_path = dirname(realpath(__file__)) + '/cmu_us_rms.flitevox'
 if __name__ == '__main__':
@@ -16,3 +17,4 @@ if __name__ == '__main__':
     stream = p.open(format=2, channels=s.channels,
                     rate=s.sample_rate, output=True)
     stream.write(s.bin())
+    sleep(0.5)
