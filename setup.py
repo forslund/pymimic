1a6dad3976a549323ff69a32587ceb2a415957ce
Åke Forslund <ake.forslund@gmail.com>
2016-09-21 07:42:21 +0200

    Version number updated to 0.2 sharp

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='pymimic',
-      version='0.2.dev',
+      version='0.2',
       packages = ['pymimic',],
       description='Python wrapper for mimic',
       author='Åke Forslund',
