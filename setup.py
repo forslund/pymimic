4e6da65e4b243319607aa74d22a16231bcb5f578
Sergio Oller <sergioller@gmail.com>
2017-09-14 11:16:24 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='pymimic',
-      version='0.3-dev',
+      version='0.4-dev0',
       packages = find_packages(),
       scripts = [os.path.join('bin', 'mimic_make_lex')],
       include_package_data=True,
@@ -13,6 +13,6 @@ setup(name='pymimic',
       author='Åke Forslund',
       author_email='ake.forslund@gmail.com',
       url='https://github.com/forslund/pymimic',
-      download_url='https://github.com/forslund/pymimic/archive/0.1.1dev.tar.gz',
+      download_url='https://github.com/forslund/pymimic/archive/0.4-dev0.tar.gz',
       keywords=['tts', 'text to speech'],
       )
